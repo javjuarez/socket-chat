0cc1aaa1eb36e0761336b4962734c5e571e90ff0
Javier Juárez <sonico@ciencias.unam.mx>
2019-02-15 19:19:45 -0600

    Chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -3,21 +3,25 @@ var socket = io();
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
-	window.location = 'index.html';
-	throw new Error('El nombre y la sala son necesarios');
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
-	nombre: params.get('nombre'),
-	sala: params.get('sala')
-}
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function (resp) {
-    	console.log('Usuarios conectados', resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -30,7 +34,7 @@ socket.on('disconnect', function() {
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -38,14 +42,19 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-    console.log('Servidor:', mensaje);
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-	console.log(personas);
+    console.log('Personas conectadas:', personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
-socket.on('mensajePrivado', function (mensaje) {
-	console.log('Mensaje privado: ', mensaje);
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('Mensaje Privado:', mensaje);
 });
